d84d0abe38848ed6196b808f5f4abc702d3a9c3e
GoldenGlint <darhgo@gmail.com>
2026-09-12 19:44:33 -0400

    Plan for CV

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+
+function ResumeHeader(){
+
+}
+
+function ResumeEucation(){
+
+}
+
+function ResumeExperience(){
+  
+}
+
+function Resume(){
+
+}
+
+function App() {
+ 
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/plan.md
@@ -0,0 +1,14 @@
+Components:
+function App(){
+    function Resume(){
+        function ResumeGeneral(){}
+        function ResumeEducation(){}
+        function ResumeExperience(){}
+    }
+    function Form(){
+        function FormGeneral(){}
+        function FormEducation(){}
+        function FormExperience(){}
+    }
+
+}
