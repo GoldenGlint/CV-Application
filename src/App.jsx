cea8b40ab09b1b9d719ead16b491b77dbb0350e6
GoldenGlint <darhgo@gmail.com>
2026-09-24 23:20:25 -0400

    Print Button and some styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import { useRef } from "react";
+import { useReactToPrint } from "react-to-print";
 import heroImg from './assets/hero.png'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
@@ -357,14 +359,39 @@ function App() {
 
   console.log(person);
 
+  const resumeRef = useRef(null);
+
+  const handlePrint = useReactToPrint({
+    contentRef: resumeRef,
+    documentTitle: "Resume",
+  });
+
   return (
-    
-    <div className="App">
-      <Form setPerson={setPerson} education={person.education} experience={person.experience}/>
-      <Resume person={person}/>
+  <div className="App">
+
+    <Form
+      setPerson={setPerson}
+      education={person.education}
+      experience={person.experience}
+    />
+
+      <div className="resumeWrapper">
+
+        <button
+          className="printResumeButton"
+          onClick={() => window.print()}
+        >
+          Print
+        </button>
+
+        <div className="resume">
+          <Resume person={person}/>
+        </div>
+
+      </div>
+
     </div>
-    
-  )
+  );
 }
 
 export default App
